e73fd6f8d3c218da2605bbfd2ee8ca090b4237f5
ikeda kohei <tdn.suama@gmail.com>
2017-08-07 22:47:45 +0900

    fix #1

--- parseJSON/parseJSON.cpp
+++ parseJSON/parseJSON.cpp
@@ -10,17 +10,17 @@
 
 #define JSON2STRUCT_STR(_json, _struct, _key, _size)						\
 do{																			\
-	strncpy(_struct.##_key, json_object_dotget_string(_json, #_key),_size); \
+	strncpy(_struct._key, json_object_dotget_string(_json, #_key),_size); \
 } while (0);																\
 
 #define JSON2STRUCT_NUM(_json, _struct, _key)					\
 do {															\
-	_struct.##_key = json_object_dotget_number(_json, #_key);	\
+	_struct._key = json_object_dotget_number(_json, #_key);	\
 }while(0); 
 
 #define JSON2STRUCT_BOOL(_json, _struct, _key)					\
 do {															\
-	_struct.##_key = json_object_dotget_boolean(_json, #_key);	\
+	_struct._key = json_object_dotget_boolean(_json, #_key);	\
 }while(0); 
 
 
@@ -33,6 +33,7 @@ typedef struct _PERSONINFO {
 	Name name;
 	unsigned int age;
 }PERSON_INFO;
+
 
 int main(void)
 {
